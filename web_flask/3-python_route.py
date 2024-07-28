fca631efc8b25da20ae687684d228ec0f3043310
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-28 20:27:22 +0300

    python_route

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Starts a Flash Web Application Python is Cool.
+"""
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define the route for the root URL, ensure strict_slashes is set to False
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Function to handle requests to the root URL ('/').
+    Returns a simple greeting message.
+    """
+    return "Hello HBNB!"
+
+# Define the route for the '/hbnb' URL, ensure strict_slashes is set to False
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Function to handle requests to the '/hbnb' URL.
+    Returns a simple message
+    """
+    return "HBNB"
+
+# Define the route for the '/c/<text>' URL, ensure strict_slashes is set to False
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Function to handle requests to the '/c/<text>' URL.
+    Displays "C " followed by the value of the text variable.
+    Underscore symbols in text are replaced with spaces.
+    """
+    return "C " + text.replace('_', ' ')
+
+# Main block to run the Flask app
+if __name__ == '__main__':
+    # Run the Flask app on 0.0.0.0:5000
+    app.run(host='0.0.0.0', port=5000)
