4259d34defcfa3433a05e98832e63c4bf7447ae0
Musyoki Dominic <124082859+dominic6809@users.noreply.github.com>
2024-07-28 20:14:54 +0300

    hello_route

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+starting a flask application
+"""
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define the route for the root URL, ensure strict_slashes is set to False
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Function to handle requests to the root URL ('/').
+    Returns a simple greeting message
+    """
+    return "Hello HBNB!"
+
+# Main block to run the Flask app
+if __name__ == '__main__':
+    # Run the Flask app on 0.0.0.0:5000
+    app.run(host='0.0.0.0', port=5000)
