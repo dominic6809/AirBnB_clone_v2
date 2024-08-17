5b508e724da7f30a48e3d6886a2c060035109f7b
dominic6809 <muuomusyoki018@gmail.com>
2024-08-17 04:43:32 -0700

    updating airbnb v2 unittest changes

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] is '{' and pline[-1] is '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -114,9 +114,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def do_create(self, args):
-    """
-    Create an object of any class with given parameters
-    """
+        """Create an object of any class with given parameters"""
     try:
         if not args:
             raise SyntaxError()
@@ -339,6 +337,7 @@ class HBNBCommand(cmd.Cmd):
         """ Help information for the update class """
         print("Updates an object with new information")
         print("Usage: update <className> <id> <attName> <attVal>\n")
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
